657433872e7bbb15f222aae5cabd36a48f36e44e
Rohit Thakur <rohitkumarthakur6677@gmail.com>
2026-05-02 17:05:52 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ export const Footer = () => (
         </Link> */}
         <Link to="/" className="flex items-center gap-2 mb-4">
           <img
-            src="/logo.jpg"
+            src="/Logo.jpg"
             alt="Skyspeak Logo"
             className="w-10 h-10 object-contain"
           />
